1956fbb80e7d7a720b113e3652f49df18c24f6fd
mur1217 <mur1217@hotmail.com>
2016-11-21 23:51:46 +0500

    ROLLBACK

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-Rails.logger = Le.new('00b65bce-5b61-4670-bade-5dd71b20caec', :debug => true, :local => true)
-Rails.logger.warn("Look at me, I'm a warning message")
-Rails.logger.info("Look at me, I'm an info message")
-Rails.logger.debug("Look at me, I'm a debug message")
